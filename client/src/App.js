cfab6c8efa11ca358c3ebbe81ba38a555c33b104
Xkill119966 <devthureinhtun@gmail.com>
2023-12-09 14:18:50 +0630

    add 404 not found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Form from "./pages/form";
-import Success from './pages/success'
+import Success from "./pages/success";
+import NotFound from "./pages/notFound";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/success" element={<Success />} />
-
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
